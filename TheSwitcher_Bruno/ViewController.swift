25f5c5d159940f3cc5137cb8d210433184db5911
Bruno Madeira <madeirabruno624@gmail.com>
2024-09-12 17:39:21 +0100

    Initial Commit

---
+++ TheSwitcher_Bruno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSwitcher_Bruno
+//
+//  Created by Bruno Madeira on 12/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
